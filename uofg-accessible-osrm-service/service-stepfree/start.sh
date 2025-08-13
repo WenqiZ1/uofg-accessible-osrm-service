d771c940cc68a7e9cc3b17c8f8ec7fe674a23363
WenqiZ1 <wenqiz422@gmail.com>
2025-08-14 01:07:41 +0800

    start.sh

--- uofg-accessible-osrm-service/service-stepfree/start.sh
+++ uofg-accessible-osrm-service/service-stepfree/start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 set -eux
 
-# Render 会注入 PORT 环境变量；本地无则用 10000
 PORT="${PORT:-10000}"
 
-# 直接启动 osrm-routed；使用 MLD（和我们上面的 partition/customize 一致）
 exec osrm-routed \
   --algorithm mld \
   --port "${PORT}" \
