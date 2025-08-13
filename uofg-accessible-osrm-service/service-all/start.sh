bd60896258bb7f31b1d17b7956475693e99c2448
WenqiZ1 <wenqiz422@gmail.com>
2025-08-14 01:26:10 +0800

    start.sh

--- uofg-accessible-osrm-service/service-all/start.sh
+++ uofg-accessible-osrm-service/service-all/start.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/env sh
+set -eux
 
-# 前台启动 OSRM（并行/自定义数据都已经在 build 时完成）
-osrm-routed --algorithm mld /srv/map.osrm --port 5000 &
+PORT="${PORT:-10000}"
 
-# 前台运行 nginx（作为 Render 对外的 10000 端口）
-nginx -g 'daemon off;'
+exec osrm-routed \
+  --algorithm mld \
+  --port "${PORT}" \
+  /srv/map.osrm
