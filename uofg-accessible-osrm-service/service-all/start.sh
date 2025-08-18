0b27a3fe6f35acccbb07e9fe927925ac0053e7c7
WenqiZ1 <wenqiz422@gmail.com>
2025-08-18 23:31:17 +0800

    start.sh

--- uofg-accessible-osrm-service/service-all/start.sh
+++ uofg-accessible-osrm-service/service-all/start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 set -eux
-
 PORT="${PORT:-10000}"
+# 打印签名，便于在日志中确认确实跑的是 ALL ACCESS
+md5sum /srv/foot.lua /srv/all_access.osm || true
+head -n 20 /srv/foot.lua || true
 
-exec osrm-routed \
-  --algorithm mld \
-  --port "${PORT}" \
-  /srv/map.osrm
-
+exec osrm-routed --algorithm mld --port "$PORT" /srv/all_access.osrm
