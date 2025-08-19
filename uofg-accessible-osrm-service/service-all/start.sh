b1ed091c1c6c3ba4a51be74e49298a5aaa1d8ee2
WenqiZ1 <wenqiz422@gmail.com>
2025-08-19 12:13:29 +0800

    Update start.sh

--- uofg-accessible-osrm-service/service-all/start.sh
+++ uofg-accessible-osrm-service/service-all/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eux
 md5sum /srv/foot.lua /srv/*.osm* || true
-head -n 10 /srv/foot.lua || true
+head -n 12 /srv/foot.lua || true
 PORT="${PORT:-10000}"
 exec osrm-routed --algorithm mld --port "$PORT" /srv/all_access.osrm
